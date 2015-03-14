9ec453006604a406fa6d8eb6b983cb65119ba428
Robert Elroy <relroy5499@gmail.com>
2015-03-13 22:37:50 -0500

    added sign up, in, and out, added all boats to application page.

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -6,9 +6,9 @@ class BoatsController < ApplicationController
 
 	def show
  		@boat = Boat.find(params[:id])
- 		if @boat.shares_possible < 1
- 			flash[:notice] = "Sorry, all the shares for this vessel are taken at this time. Please check back soon."
- 		end
+ 		# if @boat.shares_possible < 1
+ 		# 	flash[:notice] = "Sorry, all the shares for this vessel are taken at this time. Please check back soon."
+ 		# end
 	end
 
 
@@ -16,8 +16,12 @@ class BoatsController < ApplicationController
 	end
 
 	def create
-		boat = Boat.create({:image_url => params[:image_url], :name => params[:name], :make => params[:make],:size => params[:size], :harbor => params[:harbor], :shares_possible => params[:shares_possible], :full_share_price => params[:full_share_price], :two_thirds_share_price => params[:two_thirds_share_price], :half_day_credits => params[:half_day_credits], :full_day_credits => params[:full_day_credits], :multi_day_credits => 
-			params[:milti_day_credits]}) 
+		boat = Boat.create({:image_url => params[:image_url], :name => params[:name], :make => params[:make],:size => params[:size], :harbor => params[:harbor], :shares_possible => params[:shares_possible], :full_share_price => params[:full_share_price], :two_thirds_share_price => params[:two_thirds_share_price], :half_credit_AM_MTWTh => params[:half_credit_AM_MTWTh], :half_credit_PM_MTWTh => params[:half_credit_PM_MTWTh], :half_credit_AM_F => 
+			params[:half_credit_AM_F],:half_credit_PM_F => params[:half_credit_PM_F], :half_credit_AM_SAT => 
+			params[:half_credit_AM_SAT],:half_credit_PM_SAT => params[:half_credit_PM_SAT], :half_credit_AM_SUN => 
+			params[:half_credit_AM_SUN],:half_credit_PM_SUN => params[:half_credit_PM_SUN], :full_credit_MTWTh => 
+			params[:full_credit_MTWTh],:full_credit_F => params[:full_credit_F], :full_credit_SAT => 
+			params[:full_credit_SAT],:full_credit_SUN => params[:full_credit_SUN]}) 
 		flash[:success] = "Boat added"
 		redirect_to '/boats' 
 	end
@@ -30,7 +34,12 @@ class BoatsController < ApplicationController
 
 	def update
 		@boat = Boat.find(params[:id])
-		@boat.update({:image_url => params[:image_url], :name => params[:name], :make => params[:make], :size => params[:size], :harbor => params[:harbor], :shares_possible => params[:shares_possible], :full_share_price => params[:full_share_price], :two_thirds_share_price => params[:two_thirds_share_price], :half_day_credits => params[:half_day_credits], :full_day_credits => params[:full_day_credits], :multi_day_credits => params[:milti_day_credits]})
+		@boat.update({:image_url => params[:image_url], :name => params[:name], :make => params[:make],:size => params[:size], :harbor => params[:harbor], :shares_possible => params[:shares_possible], :full_share_price => params[:full_share_price], :two_thirds_share_price => params[:two_thirds_share_price], :half_credit_AM_MTWTh => params[:half_credit_AM_MTWTh], :half_credit_PM_MTWTh => params[:half_credit_PM_MTWTh], :half_credit_AM_F => 
+			params[:half_credit_AM_F],:half_credit_PM_F => params[:half_credit_PM_F], :half_credit_AM_SAT => 
+			params[:half_credit_AM_SAT],:half_credit_PM_SAT => params[:half_credit_PM_SAT], :half_credit_AM_SUN => 
+			params[:half_credit_AM_SUN],:half_credit_PM_SUN => params[:half_credit_PM_SUN], :full_credit_MTWTh => 
+			params[:full_credit_MTWTh],:full_credit_F => params[:full_credit_F], :full_credit_SAT => 
+			params[:full_credit_SAT],:full_credit_SUN => params[:full_credit_SUN]})
 		flash[:info] = "Update Complete"
 		redirect_to "/boats/#{@boat.id}" 
 
