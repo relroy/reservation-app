206d378de823a55615e6fd2ee9967e622b27a863
Robert Elroy <relroy5499@gmail.com>
2015-03-14 12:49:35 -0500

    moved sign in requirement to edit and new controller actions.

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -13,6 +13,9 @@ class BoatsController < ApplicationController
 
 
 	def new
+		if user_signed_in? && current_user.admin?
+			else render :file => "/public/404.html", :status => 404
+		end
 	end
 
 	def create
@@ -28,7 +31,10 @@ class BoatsController < ApplicationController
 
 
 	def edit
-		@boat = Boat.find(params[:id])
+		if user_signed_in? && current_user.admin?
+			@boat = Boat.find(params[:id])
+			else render :file => "/public/404.html", :status => 404
+		end
 	end
 
 
