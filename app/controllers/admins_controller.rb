b293533cba1f86dc1b1ce77e9bd3aa7c12d2500a
Robert Elroy <relroy5499@gmail.com>
2015-03-16 13:52:01 -0500

    added link to admins page in applications html, added user if statement to admins controller.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,10 @@
 class AdminsController < ApplicationController
 
   def index
+    if user_signed_in? && current_user.admin?
     @users = User.all
+    else render :file => "/public/422.html", :status => 422
+    end
     
   end
 
