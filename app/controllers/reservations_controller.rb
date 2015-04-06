ef2ec55981a65b355fd21e1eb6d6b5804c1c603f
Robert Elroy <relroy5499@gmail.com>
2015-04-06 17:40:12 -0500

    working on reducing credits as reservation is complete.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -56,6 +56,8 @@ class ReservationsController < ApplicationController
   def show
     @reservation = Reservation.find(params[:id])
 
+    # @day = Date.parse(@reservation.date_reserved.to_s)
+
     response = Weather.lookup(12784296, Weather::Units::FARENHEIT)
     @temp = response.condition.temp
     @conditions = response.condition.text
@@ -67,6 +69,12 @@ class ReservationsController < ApplicationController
   end
 
   def new
+
+    # @day = Date.parse(Date.today.to_s)
+    # @day.wday
+
+    @boat = current_user.group.boat
+    @credits = current_user.group.credits
 
     @reservations = Reservation.all
     @reservations_by_date = @reservations.group_by(&:date_reserved)
@@ -95,21 +103,37 @@ class ReservationsController < ApplicationController
 
   def create
 
+     @boat = current_user.group.boat
+     @date = Date.today
+    @current_credits = current_user.group.credits
+
     @reservation = Reservation.create({:date_reserved => params[:date_reserved], :user_id => params[:user_id], :group_id => params[:group_id], :am_block => params[:am_block], :pm_block => params[:pm_block], :full_day_block => params[:full_day_block]})
-    if @reservation.save
-         
+
+    @day = Date.parse(@reservation.date_reserved.to_s)
+    @day = @day.wday
+    puts @day.class
+  
+    if @reservation.save && @reservation.am_block? 
+      @new_credit_total = (@current_credits - @boat.half_credit_AM_MTWTh)
+    elsif @reservation.save && @reservation.pm_block?
+      @new_credit_total = (@current_credits - @boat.half_credit_PM_MTWTh)
+    elsif @reservation.save && @reservation.full_day_block?
+      @new_credit_total = (@current_credits - @boat.full_credit_MTWTh)
+    else 
+      
+    
+    end
+    puts @new_credit_total 
+
+    @group = current_user.group
+    @group.update({:credits => @new_credit_total})
+
+
     flash[:success] = "Reservation added"
     redirect_to '/reservations'
-    end
+    
 
-    response = Weather.lookup(12784296, Weather::Units::FARENHEIT)
-    @temp = response.condition.temp
-    @conditions = response.condition.text
-    @image = response.image.url
-    @datenow = response.condition.date.strftime('%A, %b %d')
-    @sunset = response.astronomy.sunset.strftime('%I:%M: %p')
-    @forecasts = response.forecasts
-    @wind = response.wind
+    
     
   end
 
